4069418d1b266a010147f462284b57ae010b71f8
Oleh Klymenko <oklymenko@deloitte.com>
2019-05-02 17:24:07 +0300

    Small changes

--- tests/try_liq.py
+++ tests/try_liq.py
@@ -1,37 +1,42 @@
-import subprocess
-import psycopg2
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+# import subprocess
+# import psycopg2
+# from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import os
 
 
 script_dir = os.path.dirname(__file__)
 
-LIQUIBASE_COMMAND = """java -jar %s \
-    --driver=%s \
-    --classpath=%s \
-    --changeLogFile=%s \
-    --url="%s" \
-    --username=%s \
-    --password=%s \
-    --logLevel=info \
+LIQUIBASE_COMMAND = """
+    sudo {} --driver={} --classpath={} --changeLogFile={} --url={} --username={} --password={} --logLevel=info {}
 """
 
-db_info = {
-        'url': "jdbc:postgresql://localhost/test_db",
-        'driver': "org.postgresql.Driver",
-        'db_connector': os.path.join(script_dir, "../migrations/jdbcdrivers/postgresql-42.2.5.jar"),
-        'command': LIQUIBASE_COMMAND
-}
+# db_info = {
+#     'url': "jdbc:postgresql://localhost/test_db",
+#     'driver': "org.postgresql.Driver",
+#     'db_connector': os.path.join(script_dir, "../migrations/jdbcdrivers/postgresql-42.2.5.jar"),
+#     'command': LIQUIBASE_COMMAND
+# }
 
-liquibase_command = db_info['command'] % (
-    os.path.join(script_dir, "../migrations/liquibase.jar"),
-    db_info['driver'],
-    db_info['db_connector'],
+liquibase_command = LIQUIBASE_COMMAND.format(
+    os.path.join(script_dir, "../migrations/liquibase"),
+    "org.postgresql.Driver",
+    os.path.join(script_dir, "../migrations/jdbcdrivers/postgresql-42.2.5.jar"),
     os.path.join(script_dir, "../migrations/changelog.xml"),
-    db_info['url'],
+    "jdbc:postgresql://localhost/test_db",
     'postgres',
-    'admin'
+    'admin',
+    "migrate"
 )
+
+# liquibase_command = db_info['command'] % (
+#     os.path.join(script_dir, "../migrations/liquibase.jar"),
+#     db_info['driver'],
+#     db_info['db_connector'],
+#     os.path.join(script_dir, "../migrations/changelog.xml"),
+#     db_info['url'],
+#     'postgres',
+#     'admin'
+# )
 
 
 # command = """./migrations/liquibase.jar --url=jdbc:postgresql://localhost/test_db \
@@ -41,24 +46,23 @@ liquibase_command = db_info['command'] % (
 # 	--password=admin \
 # 	--changeLogFile=/migrations/changelog.xml """
 
-con = psycopg2.connect(dbname='postgres',
-      user='postgres', host='localhost',
-      password='admin')
+# con = psycopg2.connect(dbname='postgres',
+#       user='postgres', host='localhost',
+#       password='admin')
+#
+# con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+#
+# cur = con.cursor()
+# try:
+#     cur.execute("DROP DATABASE {}  ;".format('test_db'))
+# except psycopg2.ProgrammingError:
+#     pass
+# finally:
+#     cur.execute("CREATE DATABASE {}  ;".format('test_db'))
+#     cur.close()
 
-con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-
-cur = con.cursor()
-try:
-    cur.execute("DROP DATABASE {}  ;".format('test_db'))
-except psycopg2.ProgrammingError:
-    pass
-finally:
-    cur.execute("CREATE DATABASE {}  ;".format('test_db'))
-    cur.close()
-
-liquibase_command = liquibase_command + "migrate"
+# liquibase_command = liquibase_command + "migrate"
 print(liquibase_command)
-import os
 os.system(liquibase_command)
 # output = subprocess.check_output(
 #     liquibase_command,
